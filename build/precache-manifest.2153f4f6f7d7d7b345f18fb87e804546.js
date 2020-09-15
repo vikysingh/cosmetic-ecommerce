d0e44e9299fd8cc0e916426114528a9ac6709a5e
Vikram Singh <51959049+vikysingh@users.noreply.github.com>
2020-09-15 08:44:23 +0200

    Add files via upload

---
+++ build/precache-manifest.2153f4f6f7d7d7b345f18fb87e804546.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d55023e69bd1d236aa0b1b3138bd5d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08e11199b30c44506b3c",
+    "url": "/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "f74c0c2c9507b8c6476e",
+    "url": "/static/css/main.50752f2f.chunk.css"
+  },
+  {
+    "revision": "08e11199b30c44506b3c",
+    "url": "/static/js/2.cfd1ed9d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cfd1ed9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f74c0c2c9507b8c6476e",
+    "url": "/static/js/main.59a2c813.chunk.js"
+  },
+  {
+    "revision": "739d510dffb38a5075c9",
+    "url": "/static/js/runtime-main.82d4f8cf.js"
+  },
+  {
+    "revision": "e8b51c454de211beb7331a1150c9b4c0",
+    "url": "/static/media/hero_bg.e8b51c45.jpg"
+  }
+]);
