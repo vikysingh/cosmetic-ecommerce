b358a38f8a0ed00297a0d1044849ab4ab2a8aa5d
Vikram Singh <51959049+vikysingh@users.noreply.github.com>
2020-10-24 10:25:25 +0200

    Add files via upload

--- src/components/Utility/Newsletter/Newsletter.jsx
+++ src/components/Utility/Newsletter/Newsletter.jsx
@@ -1,17 +1,37 @@
 import React from "react"
-import { Input } from "antd"
+import { Modal, Input } from "antd"
 
-import ThemeButton from "../Button/Button"
+//import ThemeButton from "../Button/Button"
 
 import styles from "./Newsletter.module.css"
 
-export default function Newsletter() {
+import FormValidation from "../../../utils/formValidations"
+
+function Newsletter() {
+    
+    const [ email, setEmail ] = React.useState('')
+    const inputRef = React.useRef(null)
+
+    function success() {
+        Modal.success({
+            content: 'Your subscribed successfully!',
+        });
+    }
+
     return <div className={`flexColAroundCenter ${styles.NEWSLETTER} `} >
         <h2 className="lastHeading" > Subscribe to our newsletter </h2>
         <p className="paragraph"> A short sentence describing what someone will receive </p>
         <form className="flexCenterCenter" >
-            <Input placeholder="Email" type="email" width="200" />
-            <ThemeButton> Subscribe </ThemeButton>
+            <Input placeholder="Email" type="email" width="200" ref={inputRef}
+            required={true} value={email} onChange={e => setEmail(e.target.value)} />
+            <button className="primaryActionButton" type="submit" onClick={e => {
+                e.preventDefault()
+                    new FormValidation(inputRef.current.input, success)
+                    .validateEmail(email)
+                }
+            } > Subscribe </button>
         </form>
     </div>
 }
+
+export default React.memo(Newsletter)
