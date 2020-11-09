376db9002301e7c290f46528cb41de3e546a1691
Vikram Singh <51959049+vikysingh@users.noreply.github.com>
2020-11-09 08:29:13 +0100

    Add files via upload

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -5,10 +5,7 @@ import { filterReducer, productReducer }from "../reducers"
 const store = createStore(combineReducers({
     filters: filterReducer,
     cartProducts: productReducer,
-    //cartPricing: cartReducer.pricingReducer
 }))
-
-store.subscribe(() => console.log("STORE: ", store.getState()))
 
 
 export default store
